5ce85bd13309ff52952e664dc792e435241bcff5
diegoequisde <diegoequisde@gmail.com>
2025-11-28 16:22:32 +0100

    correcciones y fin ejercicio1

--- script.js
+++ script.js
@@ -52,46 +52,40 @@ function Edificio (calle, numero, codigo) {
     // agregarPlantasYPuertas(numplantas, puertas) // Se le pasa el número de plantas que queremos crear en el piso y el número de puertas por planta.
     // Cada vez que se llame a este método, añadirá el número de plantas y puertas indicadas en los parámetros, a las que ya están creadas en el edificio.
     this.agregarPlantasYPuertas = function(numplantas, puertas) {
-        let totalPlantas = this.plantas.length();
+        let totalPlantas = this.plantas.length;
 
-        if (totalPlantas<=0) inicio = 0 
-        else {
-            inicio = totalPlantas - 1;
-        }
+        if (totalPlantas<=0) inicio = 0;
+        else inicio = totalPlantas-1;
 
-        for (i = inicio; i<totalPlantas + numplantas.length; i++) {
+        for (i = inicio; i<totalPlantas+numplantas; i++) {
             this.plantas[i] = new Array(puertas);
-            for(j=0; j<puertas; j++){
+            for(j=0; j<puertas; j++) {
                 this.plantas[i][j] = "";
             }
         }
+    };
 
     // imprimePlantas // Recorrerá el edificio e imprimirá todos los propietarios de cada puerta.
     this.imprimePlantas = function() {
         document.write("<h2>Listado de propietarios del edificio calle " + this.calle + " número " + this.numero + "</h2>");
     
-        for (i=0; i<this.plantas.length; i++){
-            for (j=0;j<this.puertas.length; j++){
-                document.write("el propietario de la planta " + (i+1) + "puerta " + (j+1) + "es " + this.plantas[i][j] + "<br/>");
+        for (i=0; i<this.plantas.length; i++) {
+            for (j=0;j<this.plantas[i].length; j++){
+                document.write("el propietario de la planta " + (i+1) + " puerta " + (j+1) + " es " + this.plantas[i][j] + "<br/>");
             }
         }
-    }
+    };
     
    // agregarPropietario(nombre,planta,puerta) // Se le pasa un nombre de propietario, un número de planta y un número de puerta y lo asignará como propietario de ese piso.
    // Cada vez que se añada un propietario a un piso de un edificio que muestre un mensaje del estilo:
        // xxxxxxxx es ahora el propietario de la puerta x de la planta x.
-    this.agregarPropietario = function(nombre,planta,puerta) {
+    this.agregarPropietario = function(nombre,planta,puerta) {  
         this.plantas[planta-1][puerta-1] = nombre;
-         document.write(nombre + "es ahora el propietario de la puerta: " + puerta + "de la planta:" + planta+"<br/>");
-    }
+        document.write(nombre + "es ahora el propietario de la puerta: " + puerta + " de la planta: " + planta + "<br/>");
     };
-}
+};
 
 /*
-Aquí se muestra un ejemplo de lo que tendría que mostrar la aplicación:
-
-Trabajando con objetos Javascript:
-
 Instanciamos 3 objetos edificioA, edificioB y edificioC con estos datos:
 
     Construido nuevo edificio en calle: Garcia Prieto, nº: 58, CP: 15706.
@@ -113,25 +107,20 @@ document.write("El código postal del edificio A es: " + edificioB.imprimeNumero
 
 
 //Agregamos 2 plantas y 3 puertas por planta al edificio A...
-
 edificioA.agregarPlantasYPuertas(2,3);
 
   /* Agregamos 4 propietarios al edificio A...
-
     Jose Antonio Lopez es ahora el propietario de la puerta 1 de la planta 1.
     Luisa Martinez es ahora el propietario de la puerta 2 de la planta 1.
     Marta Castellón es ahora el propietario de la puerta 3 de la planta 1.
     Antonio Pereira es ahora el propietario de la puerta 2 de la planta 2.
-
 */
-
 edificioA.agregarPropietario("Jose Antonio Lopez", 1, 1);
 edificioA.agregarPropietario("Luisa Martinez", 1, 2);
 edificioA.agregarPropietario("Marta Castellón", 1, 3);
-edificioA.agregarPropietario("Marta Castellón", 2, 2);
+edificioA.agregarPropietario("Antonio Pereira", 2, 2);
 
 /* Listado de propietarios del edificio calle García Prieto número 58
-
     Propietario del piso 1 de la planta 1: Jose Antonio Lopez.
     Propietario del piso 2 de la planta 1: Luisa Martinez.
     Propietario del piso 3 de la planta 1: Marta Castellón.
@@ -139,21 +128,17 @@ edificioA.agregarPropietario("Marta Castellón", 2, 2);
     Propietario del piso 2 de la planta 2: Antonio Pereira.
     Propietario del piso 3 de la planta 2:
 */
-
 edificioA.imprimePlantas();
 
 // Agregamos 1 planta más al edificio A...
-
 edificioA.agregarPlantasYPuertas(1,2);
 
 // Agregamos 1 propietario más al edificio A planta 3, puerta 2...
    // Pedro Meijide es ahora el propietario de la puerta 2 de la planta 3.
-
 edificioA.agregarPropietario("Pedro Mejide", 3, 2);
 
 /*
 Listado de propietarios del edificio calle García Prieto número 58
-
     Propietario del piso 1 de la planta 1: Jose Antonio Lopez.
     Propietario del piso 2 de la planta 1: Luisa Martinez.
     Propietario del piso 3 de la planta 1: Marta Castellón.
@@ -163,3 +148,4 @@ Listado de propietarios del edificio calle García Prieto número 58
     Propietario del piso 2 de la planta 3: Pedro Meijide.
 
 */
+edificioA.imprimePlantas();
